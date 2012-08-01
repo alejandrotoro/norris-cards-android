045b1832ac48acc436c47833ae65545fa3f563fa
Alejandro Toro <alejandrotoro@espiral.co>
2012-07-31 19:51:51 -0500

    REsolviendo conflictos

---
+++ src/com/norris/cards/ForgotPassword.java
@@ -0,0 +1,15 @@
+package com.norris.cards;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ForgotPassword extends Activity {
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password);
+    }
+
+}
